7923001786c3363aee79339fd76686d411597a97
bwj <648507447@qq.com>
2021-09-24 17:05:48 +0800

    add header link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,30 +2,32 @@ import '@elastic/eui/dist/eui_theme_amsterdam_light.css';
 import React, { useState } from 'react';
 import {
   EuiHeader,
+  EuiHeaderSection,
+  EuiHeaderSectionItem,
   EuiHeaderLogo,
-  EuiSwitch,
-  EuiSpacer,
+  EuiHeaderLink,
+  EuiHeaderLinks
 } from '@elastic/eui';
 
 export default function Header() {
-  const [position, setPosition] = useState('static');
-
-  const sections = [
-    {
-      items: [<EuiHeaderLogo>Elastic</EuiHeaderLogo>],
-      borders: 'right',
-    },
-  ];
 
   return (
     <>
-      <EuiSwitch
-        label={'Make header fixed position'}
-        checked={position === 'fixed'}
-        onChange={(e) => setPosition(e.target.checked ? 'fixed' : 'static')}
-      />
-      <EuiSpacer />
-      <EuiHeader position={position} sections={sections} />
+      <EuiHeader>
+      <EuiHeaderSectionItem border="right">
+        <EuiHeaderLogo>专利智能检索系统</EuiHeaderLogo>
+      </EuiHeaderSectionItem>
+
+      <EuiHeaderSectionItem>
+        <EuiHeaderLinks aria-label="App navigation links example">
+          <EuiHeaderLink >我的收藏</EuiHeaderLink>
+
+          <EuiHeaderLink>历史记录</EuiHeaderLink>
+
+          <EuiHeaderLink>账户管理</EuiHeaderLink>
+        </EuiHeaderLinks>
+      </EuiHeaderSectionItem>
+    </EuiHeader>
     </>
   );
 };
