ef6318de3441c4a1544e5caf267ff66c423548c1
bwj <648507447@qq.com>
2021-09-26 17:26:43 +0800

    add the search button

--- src/components/SearchBox/index.jsx
+++ src/components/SearchBox/index.jsx
@@ -1,13 +1,15 @@
-import { EuiFieldSearch} from '@elastic/eui'
+import { EuiFieldSearch, EuiButton} from '@elastic/eui'
 import React, { Component } from 'react'
 import './index.css'
 
 export default class SearchBox extends Component {
+
     render() {
         return (
             <div className='search-box'>
                 <EuiFieldSearch 
                     placeholder='Search this'
+                    append={<EuiButton fill={true} children='搜索'/>}
                 />
             </div>
         )
