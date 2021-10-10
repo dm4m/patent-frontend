2b5bbed9bee7dfb013bb2ee18dbcc798a62f3146
bwj <648507447@qq.com>
2021-10-10 09:42:52 +0800

    fix clear button in searchField, onSearch change to onKeyDown(Enter), because clear button will triger onSearch and onSearch dont't suit here

--- src/components/SearchBox/index.jsx
+++ src/components/SearchBox/index.jsx
@@ -8,30 +8,37 @@ import './index.css'
 class SearchBox extends Component {
 
     inputRef = React.createRef()
-    
+
+    searchAndJump = () =>{
+        axios.get(`http://localhost:8080/test`).then(
+            response => {
+                console.log(response.data)
+            },
+            error => { 
+            }
+        )
+        console.log(this.inputRef.current.state.value)
+        this.props.history.push(`/searchResults?input=${this.inputRef.current.state.value}`)
+    }
+
     render() {
         return (
             <div className='search-box'>
                 <EuiFieldSearch 
-                    ref= {this.inputRef} 
+                    ref={this.inputRef} 
                     placeholder='Search this'
+                    onKeyDown={
+                        (e)=>{
+                            if(e.key == 'Enter'){
+                                this.searchAndJump()
+                            }
+                        }
+                    }
+                    isClearable={true}
                     append={
                         // <Link to='/searchResults' >
                             <EuiButton  
-                                onClick={
-                                    ()=>{
-                                        axios.get(`http://localhost:8080/test`).then(
-                                            response => {
-                                                console.log(response.data)
-                                            },
-                                            error => {
-                                                
-                                            }
-                                        )
-                                        console.log(this.inputRef.current.state.value)
-                                        this.props.history.push(`/searchResults?input=${this.inputRef.current.state.value}`)
-                                    }
-                                } 
+                                onClick={this.searchAndJump} 
                                 fill={true} 
                                 children='搜索'
                             />
