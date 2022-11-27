3c667f3156fa6a19dac0444b28d9d3a720acac7d
bwj <648507447@qq.com>
2022-11-27 18:45:49 +0800

    complete fileupload

--- src/pages/NoveltyHome/index.jsx
+++ src/pages/NoveltyHome/index.jsx
@@ -1,28 +1,71 @@
 import React, { Component } from 'react'
 import axios from "axios"
-import { EuiFilePicker } from '@elastic/eui'
+import { EuiFilePicker, EuiButton } from '@elastic/eui'
 import Title from '../../components/Title'
 import './index.css'
 
 export default class NoveltyHome extends Component {
 
-    onChange = (files) => {
-        let formData = new FormData()
-        formData.append("file", files[0])
-        axios.post(
-            'http://10.108.119.71:5000/upload', 
-            formData, 
-            {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
+    state = {
+        files : [],
+        filePickerIsInvalid : false
+    }
+
+    checkFileType = (file) => {
+        if(file.type != 'application/pdf'){
+            alert("请选择.pdf格式文件上传！");
+            this.setState({
+                filePickerIsInvalid : true
+            }, ()=>{})
+            return false
+        }
+        this.setState({
+            filePickerIsInvalid : false
+        }, ()=>{})
+        return true
+    }
+
+    onChange = (upfiles) => {
+        if(upfiles.length > 0){
+            let file = upfiles[0]
+            if(this.checkFileType(file)){
+                this.setState({files : upfiles, filePickerIsInvalid : false}, ()=>{})
+            }else{
+                return;
             }
-        )
-        .then(res => {
-        console.log(res.data);
-    })
-        console.log(files);
-      };
+            
+        }else{
+            this.setState(
+                {
+                    files : [],
+                    filePickerIsInvalid : false
+                }, ()=>{}
+            )
+        }
+    }
+
+    doAnalysis(){
+        if(this.state.filePickerIsInvalid){
+            alert("请上传合规文件")
+        }else if(this.state.files.length == 0){
+            alert("请先上传待分析文件")
+        }else{
+            let formData = new FormData()
+            formData.append("file", this.state.files[0])
+            axios.post(
+                'http://10.108.119.71:5000/upload', 
+                formData, 
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            )
+                .then(res => {
+                console.log(res.data);
+            })
+        };
+    }
 
     render() {
         return (
@@ -40,9 +83,28 @@ export default class NoveltyHome extends Component {
                     name='file'
                     onChange={this.onChange}
                     onEncrypted="multipart/form-data"
+                    accept='.pdf'
+                    isInvalid={this.state.filePickerIsInvalid}
                 />
                 </div>
-                
+                <br/>
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        height: 'auto',
+                        margin: '0 auto'
+                    }}
+                >
+                    <EuiButton
+                        fill
+                        onClick={() => {
+                            this.doAnalysis()
+                        }}
+                    >
+                        进行分析
+                    </EuiButton>
+                </div>
             </div>
         )
     }
